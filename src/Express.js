8a65122c68cf493f43d76cbb5343787af558bc0a
Arpita Bhartiya <arpita.codiotic@gmail.com>
2022-02-16 18:09:34 +0530

    Todo list create and data save backand

---
+++ src/Express.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Express() {
+  return (
+    <div>Express Home page</div>
+  )
+}
---
+++ src/TodoList.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button';
+import { nanoid } from "nanoid";
+import axios from "axios"
+
+export default function TodoList() {
+  const [name, setname] = useState("");
+  const [amount, setamount] = useState("");
+  const [details, setdetails] = useState([])
+  const [id, setid] = useState(nanoid())
+  const [updateid, setupdateid] = useState("")
+
+
+  useEffect(async () => {
+    try {
+      const viewdata = await axios.get("http://localhost:8000/getdata")
+      setdetails(viewdata.data)
+    } catch (error) {
+      console.log(error);  
+    }
+
+  }, [])
+
+ 
+  
+  const HandalClick = async (e) => {
+    setid(nanoid())
+
+    if (name != "" && amount != "") {
+    try {
+      await axios.post("http://localhost:8000/postData", {
+        name,
+        amount,
+        id,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    const Alldata = { name: name, amount: amount, id: id }
+    setname("");
+    setamount('');
+   
+      setdetails((el) => {
+        return [...el, Alldata]
+      })
+  } 
+}
+
+  
+
+  const DeleteHandle =  async (elid) => {
+    try {
+      await axios.delete("http://localhost:8000/deleteData" , {data : {elid:elid}} )
+    } catch (error) {
+       console.log(error)
+    }    
+    
+    const alldata = details.filter((el) => elid != el.id)
+      setdetails(alldata)
+  }
+  
+  
+
+
+  const EditHandle =  async (id) => {
+    setupdateid(id)
+     
+  }
+
+  const  EditClick = async (e, id)=>{
+    e.preventDefault()
+  //  useEffect(async()=>{
+    try { 
+      await axios.post("http://localhost:8000/updatenew" , {
+       name,
+       amount,
+       id,
+     }) 
+     setname("");
+     setamount("");
+
+   } catch (error) {
+     console.log(error)
+   }
+  //  },[])
+  }   
+ 
+  return (
+    <>
+      <div className='d-flex justify-content-center mt-5' width=""  >
+        <div >
+          <input type="text"
+            placeholder='Name'
+            value={name}
+            onChange={(e) => setname(e.target.value)}
+            className="form-control"
+            id="Editname"
+          />
+        </div>
+        &nbsp; &nbsp;
+        <div>
+          <input type="text"
+            placeholder='Amount'
+            value={amount}
+            onChange={(e) => setamount(e.target.value)}
+            className="form-control"
+            id="Editamount"
+          />
+
+        </div>
+        &nbsp;  &nbsp; &nbsp;
+        <Button id="Editbtn" onClick={() => HandalClick()} variant="success">Save Data</Button>{' '}
+        &nbsp; &nbsp;
+        <Button id="Editupdate" onClick={(e) => EditClick(e, updateid)} variant="success">Upadate Data</Button>{' '}
+      </div>
+     <div><h4 style={{color: "red"}} className="text-center">{}</h4></div>
+
+      <div className='mt-5 d-flex justify-content-center'>
+        <table width="75%" border="1px solid">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {details.map((el) => {
+              return <tr key={el.id}>
+                <td>{el.name}</td>
+                <td>{el.amount}</td>
+                <td className='d-flex justify-content-center'>
+                  <Button className="m-0" onClick={() => DeleteHandle(el.id)} variant="danger">Delete Data</Button>{' '}
+                  &nbsp; &nbsp;
+                  <Button id="Updatebtn" className="m-0" onClick={() => EditHandle(el.id)} variant="success">Edit Data</Button>{' '}
+                </td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
